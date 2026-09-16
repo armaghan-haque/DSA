0637cc0670ec2db4a60418ae13cec3e35dda5087
Armaghan Haque <armaghan.haque.25@gmail.com>
2026-09-16 23:40:17 +0530

    first commit

---
+++ 2. JS essential /app.js
@@ -0,0 +1,27 @@
+    
+// var let const     
+    
+// const temp = 10;
+// temp = 20; // This will throw an error because 'temp' is a constant and cannot be reassigned.
+
+// const temp = 100;
+// console.log(temp); 
+
+// let temp = 100;
+// temp = 200; // This is valid because 'temp' is declared with 'let' and can be reassigned.
+// console.log(temp) ;
+
+// var temp = 100;
+// temp = 200; // This is valid because 'temp' is declared with 'var' and can be reassigned.
+// console.log(temp);  
+
+// console.log(temp);
+// var temp = 100; // This will throw a error undefined because 'temp' is accessed before its declaration due to hoisting.
+
+// console.log(temp);
+// let temp = 100; // ReferenceError: Cannot access 'temp' before initialization
+
+// sum of two integer numbers
+let a = 10;
+let b = 20;
+console.log("sum of " +a+ " + " +b+ " is " + (a+b));
---
+++ 3. Operator/app.js
@@ -0,0 +1,12 @@
+
+// Arithematic operators : + , - , * , % , /
+
+// division (/)
+// Integer/Integer is always integer
+// Math.floor() is used to get the floor value if the divison is in decimal
+// console.log(10/5);
+console.log(2%10);
+
+
+
+
